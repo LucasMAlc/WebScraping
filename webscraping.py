4ed21ac78ac2f24339ca12f98866f6ddef7f53dd
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2023-03-26 20:47:31 -0300

    imports

---
+++ webscraping.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+from time import sleep
+import csv
+
