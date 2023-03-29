3417556e558a4fdc9f2aaf550010bf7a3de8329b
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2023-03-29 07:36:14 -0300

    versão final

--- webscraping.py
+++ webscraping.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+
 import csv
 import time
 import datetime
@@ -16,7 +17,7 @@ options.add_argument('--disable-infobars')
 options.add_argument('--disable-notifications')
 driver = webdriver.Chrome(options=options)
 
-# Define as opções do navegador  e inicia o Firefox
+# Define as opções do navegador e inicia o Firefox
 
 # options = webdriver.FirefoxOptions()
 # options.add_argument('--ignore-certificate-errors')
@@ -72,14 +73,14 @@ for tipo in tipo_contratacao:
 time.sleep(1)
 driver.find_element(By.XPATH, '//*[@id="jserp-filters"]/ul/li[4]/div/div/div/button').click()
 
-time.sleep(2)
+time.sleep(1)
 experience_filter = driver.find_element(By.XPATH, '//*[@id="jserp-filters"]/ul/li[5]/div/div/button')
 experience_filter.click()
 for nivel in nivel_experiencia:
     search_experience = driver.find_element(By.XPATH, f'//*[@id="{nivel}"]')
     search_experience.click()
 
-time.sleep(4)
+time.sleep(3)
 driver.find_element(By.XPATH, '//*[@id="jserp-filters"]/ul/li[5]/div/div/div/button').click()
 
 
@@ -94,57 +95,58 @@ while True:
     try:
         job_dict = {}
         try:
-            job_dict['url'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/section/div/div[1]/div/a/h2').get_attribute('href')
+            job_dict['url'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/section/div/div[1]/div/a').get_attribute('href')
         except NoSuchElementException:
-            job_dict['url'] = None
+            job_dict['url'] = 'NULL'
         try:
             job_dict['title'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/section/div/div[1]/div/a/h2').text
         except NoSuchElementException:
-            job_dict['title'] = None
+            job_dict['title'] = 'NULL'
         try:
             job_dict['company'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/section/div/div[1]/div/h4/div[1]/span[1]/a').text
         except NoSuchElementException:
-            job_dict['company'] = None
+            job_dict['company'] = 'NULL'
         try:
-            job_dict['company_url'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/section/div/div[1]/div/a/h2').get_attribute('href')
+            job_dict['company_url'] = driver.find_element(By.XPATH, '//html/body/div[1]/div/section/div[2]/section/div/div[1]/div/h4/div[1]/span[1]/a').get_attribute('href')
         except NoSuchElementException:
-            job_dict['company_url'] = None
+            job_dict['company_url'] = 'NULL'
         try:
             job_dict['employment_type'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/div/section[1]/div/ul/li[2]/span')\
             .text.split('\n')[0]
         except NoSuchElementException:
-            job_dict['employment_type'] = None
+            job_dict['employment_type'] = 'NULL'
         try:
             job_dict['experience_level'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/div/section[1]/div/ul/li[1]/span')\
             .text.split('\n')[0]
         except NoSuchElementException:
-            job_dict['experience_level']= None
+            job_dict['experience_level']= 'NULL'
         try:
             job_dict['num_applicants'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/section/div/div[1]/div/h4/div[2]/span[2]')\
             .text.split(' ')[0]
         except NoSuchElementException:
-            job_dict['num_applicants'] = None
+            job_dict['num_applicants'] = 'NULL'
         try:
             job_dict['post_date'] = driver.find_element(By.XPATH, '/html/body/div[1]/div/section/div[2]/section/div/div[1]/div/h4/div[2]/span[1]').text
         except NoSuchElementException:
-            job_dict['post_date'] = None
+            job_dict['post_date'] = 'NULL'
+
         jobs_list.append(job_dict)
     except NoSuchElementException:
         continue
 
-    # if x+1 is None: # interrompe o loop se x+1 é None
-    if x == 5: # Limitei o número de vagas para 5 para o scraping ser mais rápido
+    if x+1 is None: # interrompe o loop se x+1 é None
         break  
 
     x += 1
 
 # Exporta os resultados para um arquivo CSV
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    filename = f'scraping-Horario_{current_time}.csv'
-    with open(filename, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=['url', 'title', 'company', 'company_url',             'employment_type','experience_level', 'num_applicants', 'post_date',])
-        writer.writeheader()
-        writer.writerows(jobs_list)
+current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+filename = f'scraping-Horario_{current_time}.csv'
+with open(filename, 'w', newline='', encoding='utf-8') as f:
+    writer = csv.DictWriter(f, fieldnames=['url', 'title', 'company', 'company_url',             'employment_type','experience_level', 'num_applicants', 'post_date',])
+    writer.writeheader()
+    writer.writerows(jobs_list)
+f.close()
 
 #Fecha o navegador
 driver.quit()
